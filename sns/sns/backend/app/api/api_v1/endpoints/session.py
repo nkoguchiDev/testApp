89800c13a578456a83c644896405c1881e67607a
nkoguchi <nkoguchi00dev@gmail.com>
2022-10-08 04:10:32 +0900

    WIP

---
+++ sns/sns/backend/app/api/api_v1/endpoints/session.py
@@ -0,0 +1,42 @@
+from typing import Any
+from datetime import timedelta
+
+from fastapi import APIRouter, HTTPException, Response, status
+from fastapi.responses import JSONResponse
+
+from app import crud, schemas
+from app.core import security
+from app.core.config import settings
+
+router = APIRouter()
+
+
+@router.post("")
+def create_session(session: schemas.SessionCreate, response: Response) -> Any:
+    """
+    OAuth2 compatible token login, get an access token for future requests
+    """
+    user = crud.user.authenticate(
+        email=session.email,
+        password=session.password)
+    if not user:
+        raise HTTPException(
+            status_code=401,
+            detail="Incorrect email or password")
+
+    if not crud.user.is_active(user):
+        raise HTTPException(status_code=400, detail="Inactive user")
+
+    access_token_expires = timedelta(
+        minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
+
+    cookie = security.create_access_token(
+        user.uuid,
+        expires_delta=access_token_expires
+    )
+    return JSONResponse(
+        status_code=status.HTTP_201_CREATED,
+        headers={
+            "set-cookie": f"session-cookie={cookie}"
+        },
+        content=None)
