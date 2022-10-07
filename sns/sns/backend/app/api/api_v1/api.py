4403077b530fc231c7c37ac491ec01397ad55cba
nkoguchi <nkoguchi00dev@gmail.com>
2022-10-08 04:10:26 +0900

    WIP

--- sns/sns/backend/app/api/api_v1/api.py
+++ sns/sns/backend/app/api/api_v1/api.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter
 
-from app.api.api_v1.endpoints import users
+from app.api.api_v1.endpoints import users, session
 
 api_router = APIRouter()
-# api_router.include_router(login.router, tags=["login"])
+api_router.include_router(session.router, prefix="/session", tags=["session"])
 api_router.include_router(users.router, prefix="/users", tags=["users"])
