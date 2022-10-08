c35017eaf619a9bfe54af0b48922387e68636125
nkoguchi <nkoguchi00dev@gmail.com>
2022-10-09 01:28:40 +0900

    WIP

--- sns/sns/backend/app/core/config.py
+++ sns/sns/backend/app/core/config.py
@@ -13,7 +13,7 @@ class Settings(BaseSettings):
     DB_HOST: str
     DB_PORT: int
     DB_NAME: str = 'test'
-    SECRET_KEY = "cce45e4c8450c2781ff1f2e1436cd61fb49c730f5b74b7b4824ca09d77eb89c3"
+    SECRET_KEY: str
     ACCESS_TOKEN_EXPIRE_MINUTES = 60 * 24 * 8
 
 
