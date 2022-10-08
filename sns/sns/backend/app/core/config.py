0d26046cb92cf9326b967171124c50c1708ea72f
nkoguchi <nkoguchi00dev@gmail.com>
2022-10-09 02:49:13 +0900

    WIP

--- sns/sns/backend/app/core/config.py
+++ sns/sns/backend/app/core/config.py
@@ -1,4 +1,5 @@
-from pydantic import BaseSettings
+from typing import List, Union
+from pydantic import BaseSettings, validator, AnyHttpUrl
 
 
 class Settings(BaseSettings):
@@ -15,6 +16,20 @@ class Settings(BaseSettings):
     DB_NAME: str = 'test'
     SECRET_KEY: str
     ACCESS_TOKEN_EXPIRE_MINUTES = 60 * 24 * 8
+
+    # BACKEND_CORS_ORIGINS is a JSON-formatted list of origins
+    # e.g: '["http://localhost", "http://localhost:4200", "http://localhost:3000", \
+    # "http://localhost:8080", "http://local.dockertoolbox.tiangolo.com"]'
+    BACKEND_CORS_ORIGINS: List[AnyHttpUrl] = ["http://localhost"]
+
+    @validator("BACKEND_CORS_ORIGINS", pre=True)
+    def assemble_cors_origins(
+            cls, v: Union[str, List[str]]) -> Union[List[str], str]:
+        if isinstance(v, str) and not v.startswith("["):
+            return [i.strip() for i in v.split(",")]
+        elif isinstance(v, (list, str)):
+            return v
+        raise ValueError(v)
 
 
 settings = Settings()
