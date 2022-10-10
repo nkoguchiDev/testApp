fbe2ac38dc9e94b855707dbf0f8a61f77f6a68d9
nkoguchi <nkoguchi00dev@gmail.com>
2022-10-10 23:25:49 +0900

    WIP

--- sns/sns/backend/setup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-echo DB_USER=root >.env
-echo DB_PASSWORD=password >>.env
-echo DB_HOST=localhost >>.env
-echo DB_PORT=27017 >>.env
-echo SECRET_KEY=$(openssl rand -hex 32) >>.env
-
-docker-compose up -d
