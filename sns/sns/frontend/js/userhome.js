9b596745ef0dcd9f31ce24f8af173539bee81d56
nkoguchi <nkoguchi00dev@gmail.com>
2022-10-13 01:28:33 +0900

    WIP

--- sns/sns/frontend/js/userhome.js
+++ sns/sns/frontend/js/userhome.js
@@ -1,8 +1,19 @@
 function getUserInformation() {
-    getData("http://localhost:80/api/v1/me").then((data) => {
+    getData("http://127.0.0.1:80/api/v1/me").then((data) => {
         if (data === false) {
-            alert("権限がありません");
+            location = "no_permission.html";
         }
+
+        const element = document.getElementById("userinfo");
+
+        const li_email = document.createElement("li");
+        li_email.textContent = "email: " + data.email;
+        element.appendChild(li_email);
+
+        // 最後の子要素として追加
+        const li_is_active = document.createElement("li");
+        li_is_active.textContent = "is_active: " + data.is_active;
+        element.appendChild(li_is_active);
     });
 }
 
