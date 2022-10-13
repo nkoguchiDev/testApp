6a971ec71c3f2c5c05b91f356fea510682a589af
nkoguchi <nkoguchi00dev@gmail.com>
2022-10-14 01:07:07 +0900

    WIP

---
+++ sns/sns/frontend/app/src/pages/Forbidden.jsx
@@ -0,0 +1,10 @@
+import { Header } from "../components/layouts/Header/Header";
+
+export const Forbidden = () => {
+    return (
+        <>
+            <Header />
+            <h1>permission denied</h1>
+        </>
+    );
+};
