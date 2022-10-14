fe3931c3c29daf6567a0f940a73039ed0deedcb8
nkoguchi <nkoguchi00dev@gmail.com>
2022-10-15 02:33:09 +0900

    WIP

--- sns/sns/frontend/app/src/App.jsx
+++ sns/sns/frontend/app/src/App.jsx
@@ -1,23 +1,5 @@
-import { BrowserRouter, Link, Routes, Route } from "react-router-dom";
-
-import { Home } from "./pages/Home";
-import { Login } from "./pages/Login";
-import { SignUp } from "./pages/SignUp";
+import { Header } from "./components/layouts/Header/Header";
 
 export const App = () => {
-    return (
-        <BrowserRouter>
-            <div className="App">
-                <Link to="/">Home</Link>
-                <Link to="/login">Login</Link>
-                <Link to="/signup">SignUp</Link>
-
-                <Routes>
-                    <Route exact path="/" element={<Home />} />
-                    <Route path="/login" element={<Login />} />
-                    <Route path="/signup" element={<SignUp />} />
-                </Routes>
-            </div>
-        </BrowserRouter>
-    );
+    return <Header />;
 };
