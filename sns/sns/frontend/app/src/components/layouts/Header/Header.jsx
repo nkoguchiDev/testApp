eae92c9c65aa97d51b7fc8c505e5bd4da9156b19
nkoguchi <nkoguchi00dev@gmail.com>
2022-10-15 02:33:18 +0900

    WIP

--- sns/sns/frontend/app/src/components/layouts/Header/Header.jsx
+++ sns/sns/frontend/app/src/components/layouts/Header/Header.jsx
@@ -1,3 +1,46 @@
+import { BrowserRouter, Link, Routes, Route } from "react-router-dom";
+import styled from "styled-components";
+
+import { Home } from "../../../pages/Home";
+import { Login } from "../../../pages/Login";
+import { SignUp } from "../../../pages/SignUp";
+
+import "./style.css";
+
 export const Header = () => {
-    return null;
+    // style
+    const HeaderBar = styled.ul`
+        text-align: center;
+        padding: 10px 0;
+        margin: 0 auto;
+    `;
+    const HeaderBarItem = styled.li`
+        list-style: none;
+        display: inline-block;
+        margin: 0 20px;
+    `;
+    // contents
+    return (
+        <BrowserRouter>
+            <div>
+                <HeaderBar>
+                    <HeaderBarItem>
+                        <Link to="/">Home</Link>
+                    </HeaderBarItem>
+                    <HeaderBarItem>
+                        <Link to="/login">Login</Link>
+                    </HeaderBarItem>
+                    <HeaderBarItem>
+                        <Link to="/signup">SignUp</Link>
+                    </HeaderBarItem>
+                </HeaderBar>
+
+                <Routes>
+                    <Route exact path="/" element={<Home />} />
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/signup" element={<SignUp />} />
+                </Routes>
+            </div>
+        </BrowserRouter>
+    );
 };
