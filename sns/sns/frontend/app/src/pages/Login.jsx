9d1cd823a0ae6505b61d643e636d061a15e34d14
nkoguchi <nkoguchi00dev@gmail.com>
2022-10-17 01:42:09 +0900

    WIP

--- sns/sns/frontend/app/src/pages/Login.jsx
+++ sns/sns/frontend/app/src/pages/Login.jsx
@@ -1,7 +1,88 @@
+import { BrowserRouter, Link, Routes, Route } from "react-router-dom";
+
+import { UserHome } from "../pages/UserHome";
+import { Forbidden } from "../pages/Forbidden";
+
 export const Login = () => {
+    function createSession() {
+        postToBackend("http://localhost:80/api/v1/session", {
+            email: document.getElementById("email").value,
+            password: document.getElementById("password").value,
+        }).then((data) => {
+            if (data) {
+                return (
+                    <Routes>
+                        <Route path="/users" element={<UserHome />} />
+                    </Routes>
+                );
+            } else {
+                return (
+                    <Routes>
+                        <Route path="/forbidden" element={<Forbidden />} />
+                    </Routes>
+                );
+            }
+        });
+    }
+
+    async function postToBackend(url = "", data = {}) {
+        const response = await fetch(url, {
+            method: "POST",
+            mode: "cors",
+            cache: "no-cache",
+            credentials: "include",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            redirect: "follow",
+            referrerPolicy: "no-referrer",
+            body: JSON.stringify(data),
+        });
+        if (response.status === 201) {
+            return true;
+        }
+        return false;
+    }
     return (
         <div>
-            <h1>Login</h1>
+            <form name="login_form">
+                <center>
+                    <h1 className="contact-title">ログイン</h1>
+                </center>
+                <center>
+                    <p>
+                        Email, Passwordご入力の上,
+                        「ログイン」ボタンをクリックしてください.
+                    </p>
+                </center>
+                <div>
+                    <center>
+                        <div>
+                            <input
+                                type="email"
+                                name="email"
+                                id="email"
+                                placeholder="Email"
+                            />
+                        </div>
+                        <br />
+                        <div>
+                            <input
+                                type="password"
+                                name="pass"
+                                id="password"
+                                placeholder="Password"
+                                onchange="validation();"
+                            />
+                        </div>
+                        <div>
+                            <button type="button" onClick={createSession}>
+                                ログイン
+                            </button>
+                        </div>
+                    </center>
+                </div>
+            </form>
         </div>
     );
 };
