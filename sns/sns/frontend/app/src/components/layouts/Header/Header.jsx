7198c1cfa24db52e2c5eedc77be72d876eb295e9
nkoguchi <nkoguchi00dev@gmail.com>
2022-10-18 00:31:20 +0900

    WIP

--- sns/sns/frontend/app/src/components/layouts/Header/Header.jsx
+++ sns/sns/frontend/app/src/components/layouts/Header/Header.jsx
@@ -1,40 +1,18 @@
-import { BrowserRouter, Link, Routes, Route } from "react-router-dom";
-import styled from "styled-components";
-
-import { AppRouter } from "../../../router";
-
-import "./style.css";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
 
 export const Header = () => {
-    // style
-    const HeaderBar = styled.ul`
-        text-align: center;
-        padding: 10px 0;
-        margin: 0 auto;
-    `;
-    const HeaderBarItem = styled.li`
-        list-style: none;
-        display: inline-block;
-        margin: 0 20px;
-    `;
-    // contents
     return (
-        <BrowserRouter>
-            <div>
-                <HeaderBar>
-                    <HeaderBarItem>
-                        <Link to="/">Home</Link>
-                    </HeaderBarItem>
-                    <HeaderBarItem>
-                        <Link to="/login">Login</Link>
-                    </HeaderBarItem>
-                    <HeaderBarItem>
-                        <Link to="/signup">SignUp</Link>
-                    </HeaderBarItem>
-                </HeaderBar>
-
-                <AppRouter />
-            </div>
-        </BrowserRouter>
+        <Box sx={{ flexGrow: 1 }}>
+            <AppBar position="static">
+                <Toolbar>
+                    <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                        ヘッダー
+                    </Typography>
+                </Toolbar>
+            </AppBar>
+        </Box>
     );
 };
