745e6d14c49a0c9b91a1df34bb5370b580047058
nkoguchi <nkoguchi00dev@gmail.com>
2022-10-20 01:01:37 +0900

    ボタンサイズを変更したいため、プロパティを追加

--- sns/sns/frontend/app/src/components/Elements/Button/Button.jsx
+++ sns/sns/frontend/app/src/components/Elements/Button/Button.jsx
@@ -2,7 +2,7 @@ import { Button } from "@mui/material";
 
 export const RichButton = (props) => (
     <div>
-        <Button variant="outlined" onClick={props.onClick}>
+        <Button variant="outlined" size="small" onClick={props.onClick}>
             {props.type}
         </Button>
     </div>
