06ba25989e7f3e80920982c4ab49b38bccee399c
nkoguchi <nkoguchi00dev@gmail.com>
2022-10-20 01:02:11 +0900

    ヘッダーをリッチにしたいため、テンプレートを適用

--- sns/sns/frontend/app/src/components/layouts/Header/Header.jsx
+++ sns/sns/frontend/app/src/components/layouts/Header/Header.jsx
@@ -2,15 +2,43 @@ import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+
+import { useNavigate } from "react-router-dom";
 
 export const Header = () => {
+    const navigate = useNavigate();
+    const mvLoginPage = () => {
+        navigate("/login");
+    };
+    const mvSignInPage = () => {
+        navigate("/signup");
+    };
+
     return (
         <Box sx={{ flexGrow: 1 }}>
             <AppBar position="static">
                 <Toolbar>
+                    <IconButton
+                        size="large"
+                        edge="start"
+                        color="inherit"
+                        aria-label="menu"
+                        sx={{ mr: 2 }}
+                    >
+                        <MenuIcon />
+                    </IconButton>
                     <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-                        ヘッダー
+                        LOGO
                     </Typography>
+                    <Button onClick={mvLoginPage} color="inherit">
+                        Login
+                    </Button>
+                    <Button onClick={mvSignInPage} color="inherit">
+                        Sign In
+                    </Button>
                 </Toolbar>
             </AppBar>
         </Box>
