8caab0279cc922685ad52f844bcb904b514fd6b5
nkoguchi <nkoguchi00dev@gmail.com>
2022-10-23 02:24:09 +0900

    WIP

--- sns/sns/frontend/app/src/pages/UserHome.jsx
+++ sns/sns/frontend/app/src/pages/UserHome.jsx
@@ -6,16 +6,17 @@ import ChatRoundedIcon from "@mui/icons-material/ChatRounded";
 import IconButton from "@mui/material/IconButton";
 
 import { HeaderOnLogin } from "../components/layouts/Header";
+import { getUserProfile } from "/app/src/features/users/api/getUserProfile";
+
+const PushButton = styled.div`
+    position: absolute;
+    right: 0;
+    bottom: 0;
+    padding: 15px;
+`;
 
 export const UserHome = () => {
     const navigate = useNavigate();
-
-    const PushButton = styled.div`
-        position: absolute;
-        right: 0;
-        bottom: 0;
-        padding: 15px;
-    `;
 
     useEffect(() => {
         return () => {
@@ -23,44 +24,28 @@ export const UserHome = () => {
         };
     });
 
-    function getUserInformation() {
-        getData("http://localhost:80/api/v1/me").then((data) => {
-            if (data === false) {
-                navigate("/forbidden");
-            }
+    const getUserInformation = () => {
+        getUserProfile().then(
+            (result) => {
+                const element = document.getElementById("userinfo");
 
-            const element = document.getElementById("userinfo");
+                const li_email = document.createElement("li");
+                li_email.textContent = "email: " + result.email;
+                element.appendChild(li_email);
 
-            const li_email = document.createElement("li");
-            li_email.textContent = "email: " + data.email;
-            element.appendChild(li_email);
+                const li_display_name = document.createElement("li");
+                li_display_name.textContent = "display name: " + result.display_name;
+                element.appendChild(li_display_name);
 
-            // 最後の子要素として追加
-            const li_is_active = document.createElement("li");
-            li_is_active.textContent = "is_active: " + data.is_active;
-            element.appendChild(li_is_active);
-        });
-    }
-
-    // POST メソッドの実装の例
-    async function getData(url = "") {
-        const response = await fetch(url, {
-            method: "GET",
-            mode: "cors",
-            cache: "no-cache",
-            credentials: "include",
-            redirect: "follow",
-            referrerPolicy: "no-referrer",
-        });
-        if (response.status === 200) {
-            return response.json();
-        }
-        return false;
-    }
-    const user = {
-        name: "unknown",
-        icon: undefined,
+                // 最後の子要素として追加
+                const li_is_active = document.createElement("li");
+                li_is_active.textContent = "is_active: " + result.is_active;
+                element.appendChild(li_is_active);
+            },
+            (error) => navigate("/forbidden")
+        );
     };
+
     return (
         <>
             <HeaderOnLogin user />
