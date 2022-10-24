d1c698002ce80987da56ad54c2dd5bb5c5850ccf
nkoguchi <nkoguchi00dev@gmail.com>
2022-10-25 00:15:30 +0900

    メッセージAPI追加

---
+++ sns/sns/backend/app/tests/crud/test_crud_message.py
@@ -0,0 +1,48 @@
+from app import crud
+from app.schemas import MessageCreate, UserCreate
+from app.tests.utils.utils import random_email, random_lower_string
+
+
+def test_create_message() -> None:
+    email = random_email()
+    password = random_lower_string()
+    display_name = random_lower_string()
+    user_in = UserCreate(
+        email=email,
+        password=password,
+        display_name=display_name)
+    user = crud.user.create(obj_in=user_in)
+
+    content = random_lower_string()
+    message_in = MessageCreate(
+        content=content)
+    message = crud.message.create(obj_in=message_in, user=user)
+    assert message.content == content
+
+
+def test_get_messages() -> None:
+    email = random_email()
+    password = random_lower_string()
+    display_name = random_lower_string()
+    user_in = UserCreate(
+        email=email,
+        password=password,
+        display_name=display_name)
+    user = crud.user.create(obj_in=user_in)
+
+    content_01 = random_lower_string()
+    message_in_01 = MessageCreate(
+        content=content_01)
+    message_01 = crud.message.create(
+        obj_in=message_in_01, user=user)
+
+    content_02 = random_lower_string()
+    message_in_02 = MessageCreate(
+        content=content_02)
+    message_02 = crud.message.create(
+        obj_in=message_in_02, user=user)
+
+    messages = crud.message.get(user=user)
+
+    assert message_01 in messages
+    assert message_02 in messages
