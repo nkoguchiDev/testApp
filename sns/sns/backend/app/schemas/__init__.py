560ee88c1f9fa3b6bee1640b9a09c773c6e4da3b
nkoguchi <nkoguchi00dev@gmail.com>
2022-10-25 00:15:05 +0900

    メッセージAPI追加

--- sns/sns/backend/app/schemas/__init__.py
+++ sns/sns/backend/app/schemas/__init__.py
@@ -1,3 +1,3 @@
 from .user import UserBase, UserCreate, UserUpdate  # noqa
 from .session import SessionCreate, SessionPayload  # noqa
-from .post_message import PostMessageCreate  # noqa
+from .message import MessageBase, MessageCreate  # noqa
