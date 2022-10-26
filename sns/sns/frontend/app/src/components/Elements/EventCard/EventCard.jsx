8980eb984595fd3c7f2ff1bd3a50e8398c4d7cb1
nkoguchi <nkoguchi00dev@gmail.com>
2022-10-27 00:34:33 +0900

    messageAPIから取得してUIに表示するように変更

--- sns/sns/frontend/app/src/components/Elements/EventCard/EventCard.jsx
+++ sns/sns/frontend/app/src/components/Elements/EventCard/EventCard.jsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import { useEffect } from "react";
 import styled from "styled-components";
 
 import Card from "@mui/material/Card";
@@ -14,56 +15,74 @@ import MoreVertIcon from "@mui/icons-material/MoreVert";
 import DeleteIcon from "@mui/icons-material/Delete";
 import Tooltip from "@mui/material/Tooltip";
 
-const CenterStyle = styled.div`
-    font-family: arial;
-    font-size: 24px;
-    margin: 25px;
-    width: 1350px;
-    height: 200px;
+import { getMessageList } from "./../../../features/messages/api/getMessageList";
+
+const CardList = styled.li`
+    display: block;
 `;
 
-export const EventCard = () => {
-    return (
-        <CenterStyle>
-            <ul>
-                <li>
-                    <Card sx={{ maxWidth: 345 }}>
-                        <CardHeader
-                            avatar={
-                                <Avatar
-                                    alt="Remy Sharp"
-                                    src={`${process.env.PUBLIC_URL}/icon.png`}
+export const EventCard = (props) => {
+    useEffect(() => {
+        return () => {
+            setMessagesToCards(props.user, props.events);
+        };
+    });
+
+    const setMessagesToCards = (user, events) => {
+        getMessageList().then(
+            (result) => {
+                const element = document.getElementById("events");
+                for (let e in events) {
+                    const card = (
+                        <CardList>
+                            <Card sx={{ maxWidth: 345 }}>
+                                <CardHeader
+                                    avatar={
+                                        <Avatar
+                                            alt="message post user name"
+                                            src={`${process.env.PUBLIC_URL}/icon.png`}
+                                        />
+                                    }
+                                    action={
+                                        <IconButton aria-label="settings">
+                                            <MoreVertIcon />
+                                        </IconButton>
+                                    }
+                                    title="message post user name"
+                                    // subheader={e.created}
+                                    subheader="date"
                                 />
-                            }
-                            action={
-                                <IconButton aria-label="settings">
-                                    <MoreVertIcon />
-                                </IconButton>
-                            }
-                            title="nkoguchiDev"
-                            subheader="Sep 14, 2016"
-                        />
-                        <CardContent>
-                            <Typography variant="body2" color="text.secondary">
-                                本文
-                            </Typography>
-                        </CardContent>
-                        <CardActions disableSpacing>
-                            <IconButton aria-label="add to favorites">
-                                <FavoriteIcon />
-                            </IconButton>
-                            <IconButton aria-label="share">
-                                <ShareIcon />
-                            </IconButton>
-                            <Tooltip title="Delete">
-                                <IconButton>
-                                    <DeleteIcon />
-                                </IconButton>
-                            </Tooltip>
-                        </CardActions>
-                    </Card>
-                </li>
-            </ul>
-        </CenterStyle>
+                                <CardContent>
+                                    <Typography variant="body2" color="text.secondary">
+                                        {e.content}
+                                    </Typography>
+                                </CardContent>
+                                <CardActions disableSpacing>
+                                    <IconButton aria-label="add to favorites">
+                                        <FavoriteIcon />
+                                    </IconButton>
+                                    <IconButton aria-label="share">
+                                        <ShareIcon />
+                                    </IconButton>
+                                    <Tooltip title="Delete">
+                                        <IconButton>
+                                            <DeleteIcon />
+                                        </IconButton>
+                                    </Tooltip>
+                                </CardActions>
+                            </Card>
+                        </CardList>
+                    );
+                    element.appendChild(card);
+                }
+            },
+            (error) => alert("error")
+        );
+    };
+
+    return (
+        <center>
+            <ul id="events" />
+        </center>
     );
 };
