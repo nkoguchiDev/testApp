932c7b54adac5d157ab1e9aafc8d2762d15552b3
nkoguchi <nkoguchi00dev@gmail.com>
2022-10-29 04:14:37 +0900

    コンポーネントのスタイルはページファイルで

--- sns/sns/frontend/app/src/components/Elements/MessageModal/MessageModal.jsx
+++ sns/sns/frontend/app/src/components/Elements/MessageModal/MessageModal.jsx
@@ -1,5 +1,4 @@
 import * as React from "react";
-import styled from "styled-components";
 import IconButton from "@mui/material/IconButton";
 import ChatRoundedIcon from "@mui/icons-material/ChatRounded";
 
@@ -12,25 +11,6 @@ import DialogContentText from "@mui/material/DialogContentText";
 import DialogTitle from "@mui/material/DialogTitle";
 
 import { createMessage } from "../../../features/messages/api/createMessage";
-
-const BoxStyle = {
-    position: "absolute",
-    top: "50%",
-    left: "50%",
-    transform: "translate(-50%, -50%)",
-    width: 400,
-    bgcolor: "background.paper",
-    border: "2px solid #000",
-    boxShadow: 24,
-    p: 4,
-};
-
-const RightLower = styled.div`
-    position: absolute;
-    right: 0;
-    bottom: 0;
-    padding: 15px;
-`;
 
 export const MessageModal = () => {
     const [open, setOpen] = React.useState(false);
@@ -45,7 +25,7 @@ export const MessageModal = () => {
     };
 
     return (
-        <RightLower>
+        <div>
             <IconButton
                 size="large"
                 aria-label="account of current user"
@@ -79,6 +59,6 @@ export const MessageModal = () => {
                     <Button onClick={pushOK}>OK</Button>
                 </DialogActions>
             </Dialog>
-        </RightLower>
+        </div>
     );
 };
