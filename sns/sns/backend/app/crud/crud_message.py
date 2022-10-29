d92e2d250e2ec18975087ed7db354297fdb4d32c
nkoguchi <nkoguchi00dev@gmail.com>
2022-10-30 01:53:31 +0900

    メッセージ機能の改修

--- sns/sns/backend/app/crud/crud_message.py
+++ sns/sns/backend/app/crud/crud_message.py
@@ -1,5 +1,5 @@
 from uuid import uuid4
-
+import datetime
 
 # from typing import Any, Dict, Optional, Union
 
@@ -17,14 +17,27 @@ class CRUDMessage(CRUDBase[Message, MessageCreate, MessageCreate]):
             self,
             obj_in: MessageCreate,
             user: User) -> Message:
+
+        t_delta = datetime.timedelta(hours=9)
+        JST = datetime.timezone(t_delta, 'JST')
+        now = datetime.datetime.now(JST)
+
         uuid = uuid4().hex
         db_obj = Message(
             uuid=uuid,
             content=obj_in.content,
             user=user,
+            date=now.strftime('%Y-%m-%d %H:%M'),
+            create_user=user.display_name
         )
         db_obj.save()
         return Message.objects(uuid=uuid).first()
+
+    def delete(
+            self,
+            uuid: str,
+            user: User) -> Message:
+        return Message.objects(uuid=uuid, user=user).delete()
 
 
 message = CRUDMessage(Message)
