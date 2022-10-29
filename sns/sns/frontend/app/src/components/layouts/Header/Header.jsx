dbf349ae158e2c98f42dccf921c316c6609721d8
nkoguchi <nkoguchi00dev@gmail.com>
2022-10-30 01:53:52 +0900

    メッセージ機能の改修

--- sns/sns/frontend/app/src/components/layouts/Header/Header.jsx
+++ sns/sns/frontend/app/src/components/layouts/Header/Header.jsx
@@ -1,3 +1,7 @@
+import * as React from "react";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
@@ -5,11 +9,17 @@ import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
 import IconButton from "@mui/material/IconButton";
 import MenuIcon from "@mui/icons-material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import Menu from "@mui/material/Menu";
+import Avatar from "@mui/material/Avatar";
+import Badge from "@mui/material/Badge";
+import NotificationsIcon from "@mui/icons-material/Notifications";
 
-import { useNavigate } from "react-router-dom";
+import { getUserProfile } from "../../../features/users/api/getUserProfile";
 
-export const Header = () => {
+export const Header = (props) => {
     const navigate = useNavigate();
+
     const mvLoginPage = () => {
         navigate("/login");
     };
@@ -17,31 +27,103 @@ export const Header = () => {
         navigate("/signup");
     };
 
-    return (
-        <Box sx={{ flexGrow: 1 }}>
-            <AppBar position="static">
-                <Toolbar>
-                    <IconButton
-                        size="large"
-                        edge="start"
-                        color="inherit"
-                        aria-label="menu"
-                        sx={{ mr: 2 }}
-                    >
-                        <MenuIcon />
-                    </IconButton>
-                    <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-                        LOGO
-                    </Typography>
+    const [anchorEl, setAnchorEl] = React.useState(null);
+    const handleMenu = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
 
-                    <Button onClick={mvLoginPage} color="inherit">
-                        Login
-                    </Button>
-                    <Button onClick={mvSignInPage} color="inherit">
-                        Sign Up
-                    </Button>
-                </Toolbar>
-            </AppBar>
-        </Box>
-    );
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+
+    const LoggedHeader = (user) => {
+        return (
+            <Box sx={{ flexGrow: 1 }}>
+                <AppBar position="static">
+                    <Toolbar>
+                        <IconButton
+                            size="large"
+                            edge="start"
+                            color="inherit"
+                            aria-label="menu"
+                            sx={{ mr: 2 }}
+                        >
+                            <MenuIcon />
+                        </IconButton>
+                        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                            LOGO
+                        </Typography>
+                        <IconButton aria-label="cart">
+                            <Badge badgeContent={4} color="error" marge="">
+                                <NotificationsIcon color="action" />
+                            </Badge>
+                        </IconButton>
+                        <div>
+                            <IconButton
+                                size="large"
+                                aria-label="account of current user"
+                                aria-controls="menu-appbar"
+                                aria-haspopup="true"
+                                onClick={handleMenu}
+                                color="inherit"
+                            >
+                                <Avatar
+                                    alt={user.name ? user.name : "?"}
+                                    src={`${process.env.PUBLIC_URL}/icon.png`}
+                                />
+                            </IconButton>
+                            <Menu
+                                id="menu-appbar"
+                                anchorEl={anchorEl}
+                                anchorOrigin={{
+                                    vertical: "top",
+                                    horizontal: "right",
+                                }}
+                                keepMounted
+                                transformOrigin={{
+                                    vertical: "top",
+                                    horizontal: "right",
+                                }}
+                                open={Boolean(anchorEl)}
+                                onClose={handleClose}
+                            >
+                                <MenuItem onClick={handleClose}>Account</MenuItem>
+                                <MenuItem onClick={handleClose}>Logout</MenuItem>
+                            </Menu>
+                        </div>
+                    </Toolbar>
+                </AppBar>
+            </Box>
+        );
+    };
+    const NotLoggedHeader = () => {
+        return (
+            <Box sx={{ flexGrow: 1 }}>
+                <AppBar position="static">
+                    <Toolbar>
+                        <IconButton
+                            size="large"
+                            edge="start"
+                            color="inherit"
+                            aria-label="menu"
+                            sx={{ mr: 2 }}
+                        >
+                            <MenuIcon />
+                        </IconButton>
+                        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                            LOGO
+                        </Typography>
+
+                        <Button onClick={mvLoginPage} color="inherit">
+                            Login
+                        </Button>
+                        <Button onClick={mvSignInPage} color="inherit">
+                            Sign Up
+                        </Button>
+                    </Toolbar>
+                </AppBar>
+            </Box>
+        );
+    };
+    return props.user ? LoggedHeader(props.user) : NotLoggedHeader();
 };
