fb0f357fb7dbf165747832fbf50ca532351852f5
nkoguchi <nkoguchi00dev@gmail.com>
2022-10-30 01:53:37 +0900

    メッセージ機能の改修

--- sns/sns/backend/app/schemas/__init__.py
+++ sns/sns/backend/app/schemas/__init__.py
@@ -1,3 +1,3 @@
 from .user import UserBase, UserCreate, UserUpdate  # noqa
 from .session import SessionCreate, SessionPayload  # noqa
-from .message import MessageBase, MessageCreate  # noqa
+from .message import MessageBase, MessageCreate, MessageResponse  # noqa
