8662c4cdc8af6ade42ef861aeae397eb814f50cd
nkoguchi <nkoguchi00dev@gmail.com>
2022-12-12 08:54:49 +0900

    WIP

---
+++ kazipo/nuxt-app/app.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <NuxtWelcome />
+  </div>
+</template>
+
+<script setup>
+  // import HelloWorld from '@/components/HelloWorld.vue'
+</script>
---
+++ kazipo/nuxt-app/nuxt.config.ts
@@ -0,0 +1,10 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+    buildModules: [
+      // Simple usage
+      '@nuxtjs/vuetify',
+  
+      // With options
+      ['@nuxtjs/vuetify', { /* module options */ }]
+    ]
+  })
