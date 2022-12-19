26f4bd3e0480a7bd8272e0877842ed5e08828226
nkoguchi <nkoguchi00dev@gmail.com>
2022-12-19 22:54:44 +0900

    順番にアップロードしなくても良いように変更

--- fileupload/backend/app/main.py
+++ fileupload/backend/app/main.py
@@ -1,16 +1,20 @@
-from fastapi import FastAPI, File, UploadFile
+from fastapi import FastAPI, File, Form, UploadFile
 
 import os
 import uuid
-import pathlib
+import shutil
+import glob
 
 app = FastAPI()
 
 
 @app.on_event("startup")
 async def startup_event():
-    if not os.path.exists("/app/out"):  # ディレクトリが存在しないなら
-        os.mkdir("/app/out")  # 作成
+    if not os.path.exists("/app/out"):
+        os.mkdir("/app/out")
+
+    if not os.path.exists("/app/tmp"):
+        os.mkdir("/app/tmp")
 
 
 @app.post("/images")
@@ -18,7 +22,7 @@ def file_upload_reservation():
     _id = uuid.uuid4().hex
     retry = 1
     # file exist check
-    while os.path.isfile(f"/app/out/{_id}.jpg"):
+    while os.path.exists(f"/app/tmp/{_id}"):
         _id = uuid.uuid4().hex
 
         if retry == 5:
@@ -26,25 +30,41 @@ def file_upload_reservation():
 
         retry += 1
 
-    file = pathlib.Path(f"/app/out/{_id}.jpg")
-    file.touch()
+    os.mkdir(f"/app/tmp/{_id}")
+
     return {"id": _id}
 
 
 @app.post("/images/{file_id}")
-async def file_split_upload(file_id: str, file: bytes = File()):
+async def file_split_upload(file_id: str,
+                            filename: str = Form(),
+                            hash: str = Form(),
+                            chunk: UploadFile = File()):
 
-    with open(f"/app/out/{file_id}.jpg", "ab") as f2:
-        f2.write(file)
+    if chunk:
+        with open(f"/app/tmp/{file_id}/tmp_data-{hash.zfill(4)}", "wb") as f:
+            shutil.copyfileobj(chunk.file, f)
 
-    current_size = os.path.getsize(f"/app/out/{file_id}.jpg")
 
-    return {"id": file_id, "current_size": current_size}
+@app.post("/images/{file_id}/merge")
+async def file_merge(file_id: str):
+    files = glob.glob(f"/app/tmp/{file_id}/*")
+
+    if files:
+        with open(f"/app/out/{file_id}.jpg", 'wb') as fout:
+            for file in files:
+                with open(file, 'rb') as fin:
+                    fout.write(fin.read())
+
+    if os.path.exists(f"/app/tmp/{file_id}"):
+        shutil.rmtree(f"/app/tmp/{file_id}")
+
+    return {"size": os.path.getsize(f"/app/out/{file_id}.jpg")}
 
 
 @app.delete("/images/{file_id}")
 async def file_delete(file_id: str):
+
     if os.path.isfile(f"/app/out/{file_id}.jpg"):
         os.remove(f"/app/out/{file_id}.jpg")
-
     return
