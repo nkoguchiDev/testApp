35b0274a1a46eb35898077afa01538a947da969c
nkoguchi <nkoguchi00dev@gmail.com>
2022-12-19 21:08:46 +0900

    ファイルアップロード機能の追加

--- fileupload/backend/app/main.py
+++ fileupload/backend/app/main.py
@@ -1,27 +1,50 @@
 from fastapi import FastAPI, File, UploadFile
 
 import os
+import uuid
+import pathlib
 
 app = FastAPI()
 
 
-@app.post("/files/")
-async def create_file(file: bytes = File()):
-    return {"file_size": len(file)}
-
-
-@app.post("/uploadfile/")
-async def create_upload_file(file: UploadFile):
-    return {"filename": 0}
-
-
-@app.post("/")
-async def file_split_upload(file: bytes = File()):
+@app.on_event("startup")
+async def startup_event():
     if not os.path.exists("/app/out"):  # ディレクトリが存在しないなら
         os.mkdir("/app/out")  # 作成
-    """
-    pathをバイナリファイルとして開きdataを書き込む
-    """
-    with open("/app/out/data", 'ab') as fout:
-        fout.write(file)
-    return {"file_size": len(file)}
+
+
+@app.post("/images")
+def file_upload_reservation():
+    _id = uuid.uuid4().hex
+    retry = 1
+    # file exist check
+    while os.path.isfile(f"/app/out/{_id}.jpg"):
+        _id = uuid.uuid4().hex
+
+        if retry == 5:
+            return {"error": {"message": "please retry"}}
+
+        retry += 1
+
+    file = pathlib.Path(f"/app/out/{_id}.jpg")
+    file.touch()
+    return {"id": _id}
+
+
+@app.post("/images/{file_id}")
+async def file_split_upload(file_id: str, file: bytes = File()):
+
+    with open(f"/app/out/{file_id}.jpg", "ab") as f2:
+        f2.write(file)
+
+    current_size = os.path.getsize(f"/app/out/{file_id}.jpg")
+
+    return {"id": file_id, "current_size": current_size}
+
+
+@app.delete("/images/{file_id}")
+async def file_delete(file_id: str):
+    if os.path.isfile(f"/app/out/{file_id}.jpg"):
+        os.remove(f"/app/out/{file_id}.jpg")
+
+    return
