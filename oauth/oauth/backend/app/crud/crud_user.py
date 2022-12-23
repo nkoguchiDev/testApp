83e0d2768f8e8d80eed826c16690d23ebad3abfe
nkoguchi <nkoguchi00dev@gmail.com>
2022-12-23 21:51:21 +0900

    hash化関数の名前を変更

--- oauth/oauth/backend/app/crud/crud_user.py
+++ oauth/oauth/backend/app/crud/crud_user.py
@@ -6,7 +6,7 @@ from typing import Any, Dict, Optional, Union
 from app.crud.base import CRUDBase
 from app.models.user import User
 from app.schemas.user import UserCreate, UserUpdate
-from app.core.security import get_password_hash, verify_password
+from app.core.security import get_hash_string, verify_password
 
 
 class CRUDUser(CRUDBase[User, UserCreate, UserUpdate]):
@@ -19,7 +19,7 @@ class CRUDUser(CRUDBase[User, UserCreate, UserUpdate]):
         db_obj = User(
             uuid=uuid,
             email=obj_in.email,
-            hashed_password=get_password_hash(obj_in.password),
+            hashed_password=get_hash_string(obj_in.password),
             display_name=obj_in.display_name,
             is_active=obj_in.is_active,
             is_superuser=obj_in.is_superuser,
@@ -36,7 +36,7 @@ class CRUDUser(CRUDBase[User, UserCreate, UserUpdate]):
         else:
             update_data = obj_in.dict(exclude_unset=True)
         if update_data["password"]:
-            hashed_password = get_password_hash(update_data["password"])
+            hashed_password = get_hash_string(update_data["password"])
             del update_data["password"]
             update_data["hashed_password"] = hashed_password
 
