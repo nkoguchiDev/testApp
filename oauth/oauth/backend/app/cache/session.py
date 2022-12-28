35f33d9dc3136b30f9c01abc903cc42bfbd99f51
nkoguchi <nkoguchi00dev@gmail.com>
2022-12-28 23:47:26 +0900

    WIP

--- oauth/oauth/backend/app/cache/session.py
+++ oauth/oauth/backend/app/cache/session.py
@@ -2,4 +2,4 @@ import redis
 
 from app.core.config import settings
 
-client = redis.Redis(host=settings.CACHE_HOST, port=settings.CACHE_PORT, db=0)
+cache = redis.Redis(host=settings.CACHE_HOST, port=settings.CACHE_PORT, db=0)
