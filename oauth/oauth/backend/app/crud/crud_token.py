48638104b1a9c3b396fb4a9cddca3c4a67d88475
nkoguchi <nkoguchi00dev@gmail.com>
2022-12-30 23:39:46 +0900

    token取得削除機能の追加

--- oauth/oauth/backend/app/crud/crud_token.py
+++ oauth/oauth/backend/app/crud/crud_token.py
@@ -9,10 +9,17 @@ from app.core.security import create_oauth_token
 
 class CRUDToken(CRUDBase[Token, TokenCreate, TokenUpdate]):
 
-    def create(self, cache) -> str:
+    def get(self, cache, token: str) -> dict:
+        return cache.hgetall(token)
+
+    def create(self, cache, user_id: str) -> str:
         oauth_token = create_oauth_token()
-        cache.set(oauth_token, {"user_id": "id"})
+        cache.hmset(oauth_token, {"user_id": user_id}, ex=60)
         return oauth_token
+
+    def delete(self, cache, token: str) -> None:
+        cache.delete(token)
+        return
 
 
 token = CRUDToken(Token)
