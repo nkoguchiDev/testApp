1b916864986bb479adcb0007b78e328f7e59b1bc
nkoguchi <nkoguchi00dev@gmail.com>
2023-01-11 23:05:36 +0900

    レイアウト変更

--- flet_app/app/todo/main.py
+++ flet_app/app/todo/main.py
@@ -1,31 +1,46 @@
 import flet as ft
 
 
+class TodoApp(ft.UserControl):
+    def build(self):
+        self.new_task = ft.TextField(
+            hint_text="Whats needs to be done?", expand=True)
+        self.tasks = ft.Column()
+
+        # application's root control (i.e. "view") containing all other
+        # controls
+        return ft.Column(
+            width=600,
+            controls=[
+                ft.Row(
+                    controls=[
+                        self.new_task,
+                        ft.FloatingActionButton(
+                            icon=ft.icons.ADD,
+                            on_click=self.add_clicked),
+                    ],
+                ),
+                self.tasks,
+            ],
+        )
+
+    def add_clicked(self, e):
+        self.tasks.controls.append(ft.Checkbox(label=self.new_task.value))
+        self.new_task.value = ""
+        self.update()
+
+
 def main(page: ft.Page):
-    def add_clicked(e):
-        tasks_view.controls.append(ft.Checkbox(label=new_task.value))
-        new_task.value = ""
-        view.update()
-
-    new_task = ft.TextField(hint_text="Whats needs to be done?", expand=True)
-    tasks_view = ft.Column()
-    view = ft.Column(
-        width=600,
-        controls=[
-            ft.Row(
-                controls=[
-                    new_task,
-                    ft.FloatingActionButton(
-                        icon=ft.icons.ADD,
-                        on_click=add_clicked),
-                ],
-            ),
-            tasks_view,
-        ],
-    )
-
+    page.title = "ToDo App"
     page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
-    page.add(view)
+    page.update()
+
+    # create application instance
+    app1 = TodoApp()
+    app2 = TodoApp()
+
+    # add application's root control to the page
+    page.add(app1, app2)
 
 
 ft.app(target=main)
