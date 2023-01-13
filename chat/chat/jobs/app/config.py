da39def3e1db04cc21e5b66d21a9ade122589aff
nkoguchi <nkoguchi00dev@gmail.com>
2023-01-13 23:12:01 +0900

    api連携用設定追加

--- chat/chat/jobs/app/config.py
+++ chat/chat/jobs/app/config.py
@@ -5,5 +5,15 @@ class Settings(BaseSetting):
     db_host: str
     db_port: int
 
+    voice_api_host: str = "localhost"
+    voice_api_port: int = 80
+    voice_api_protocol: str = "http"
+
+    voice_query_endpoint: str \
+        = f"{voice_api_protocol}://{voice_api_host}:{voice_api_port}/audio_query"
+
+    voice_generator_endpoint: str \
+        = f"{voice_api_protocol}://{voice_api_host}:{voice_api_port}/synthesis"
+
 
 settings = Settings()
