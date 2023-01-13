dfdb2887093b6be42b062b7628f9fef8c26b7db6
nkoguchi <nkoguchi00dev@gmail.com>
2023-01-13 23:12:12 +0900

    音声作成関数追加

--- chat/chat/jobs/app/gen.py
+++ chat/chat/jobs/app/gen.py
@@ -1,5 +1,19 @@
 import requests
 
+from config import settings
+
 
 def generate_voice(text: str):
-    requests.post()
+    query = requests.post(
+        url=settings.voice_query_endpoint,
+        params={"speaker": 1, "text": text})
+
+    voice_wav = requests.post(
+        url=settings.voice_generator_endpoint,
+        params={
+            "speaker": 1},
+        json=query.json(),
+        headers={"Content-Type": "application/json"})
+
+    with open('./app/voice/iojdiwajdwa.wav', 'wb') as f:
+        f.write(voice_wav.content)
