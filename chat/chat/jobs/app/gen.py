4ec47de8709fa7acebd515bd7f98c9250ccf56e4
nkoguchi <nkoguchi00dev@gmail.com>
2023-01-14 23:31:02 +0900

    返答作成用openai連携用クラスの作成

--- chat/chat/jobs/app/gen.py
+++ chat/chat/jobs/app/gen.py
@@ -1,4 +1,5 @@
 import requests
+import openai
 
 from config import settings
 
@@ -17,3 +18,21 @@ def generate_voice(text: str):
 
     with open('./app/voice/iojdiwajdwa.wav', 'wb') as f:
         f.write(voice_wav.content)
+
+
+class AIChat:
+    def __init__(self):
+        # ※冒頭で作成したopenai の APIキーを設定してください
+        openai.api_key = settings.gpt_api_key
+
+    def response(self, user_input):
+        # openai の GPT-3 モデルを使って、応答を生成する
+        response = openai.Completion.create(
+            engine="text-davinci-002",  # text-davinci-003 を指定した方がより自然な文章が生成されます
+            prompt=user_input,
+            max_tokens=1024,
+            temperature=0.5,  # 生成する応答の多様性
+        )
+
+        # 応答のテキスト部分を取り出して返す
+        return response['choices'][0]['text']
