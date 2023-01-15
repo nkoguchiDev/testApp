0492936ca0b761f34573b4696c7f04c2b1d8de64
nkoguchi <nkoguchi00dev@gmail.com>
2023-01-15 23:57:30 +0900

    学習用テキストを定義

--- chat/chat/jobs/app/gen.py
+++ chat/chat/jobs/app/gen.py
@@ -24,33 +24,23 @@ class AIChat:
     def __init__(self):
         # ※冒頭で作成したopenai の APIキーを設定してください
         openai.api_key = settings.gpt_api_key
-        self.message = "ME:これより先は語尾を「のだ」または「なのだ」で続けてください。"
+        self.message = "ME:これより先は語尾を「のだ」または「なのだ」にしてください。\nAI:了解なのだ。よろしくなのだ"
 
     def response(self, user_input):
         # openai の GPT-3 モデルを使って、応答を生成する
-        self.message = f"{self.message}\nAI:"
+        self.message = f"{self.message}\nME:{user_input}"
         print(self.message)
         response = openai.Completion.create(
             engine="text-davinci-003",
-            prompt=user_input,
+            prompt=self.message,
             max_tokens=100,
             temperature=0.7,  # 生成する応答の多様性
             frequency_penalty=0.2,
             presence_penalty=0,
         )
-        self.message = f"{self.message}\n{response['choices'][0]['text']}\nME:{user_input}"
-        print(self.message)
-        response = openai.Completion.create(
-            engine="text-davinci-003",
-            prompt=user_input,
-            max_tokens=100,
-            temperature=0.7,  # 生成する応答の多様性
-            frequency_penalty=0.2,
-            presence_penalty=0,
-        )
-
+        response = response['choices'][0]['text'].replace("\n", "")
         # 応答のテキスト部分を取り出して返す
-        return response['choices'][0]['text']
+        return response
 
 
 if __name__ == "__main__":
