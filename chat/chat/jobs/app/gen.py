bbeec85a681bf388ca07e7be8b4f2197ea068356
nkoguchi <nkoguchi00dev@gmail.com>
2023-01-16 22:43:20 +0900

    会話の履歴をファイルに保持する

--- chat/chat/jobs/app/gen.py
+++ chat/chat/jobs/app/gen.py
@@ -24,26 +24,35 @@ class AIChat:
     def __init__(self):
         # ※冒頭で作成したopenai の APIキーを設定してください
         openai.api_key = settings.gpt_api_key
-        self.message = "ME:これより先は語尾を「のだ」または「なのだ」にしてください。\nAI:了解なのだ。よろしくなのだ"
+        self.message = ""
 
     def response(self, user_input):
+        with open("./app/history.txt", 'r') as f:
+            self.message = f.read()
+
+        prompt = f"{self.message}\nME:{user_input}"
+
         # openai の GPT-3 モデルを使って、応答を生成する
-        self.message = f"{self.message}\nME:{user_input}"
-        print(self.message)
         response = openai.Completion.create(
             engine="text-davinci-003",
-            prompt=self.message,
-            max_tokens=100,
+            prompt=prompt,
+            max_tokens=1000,
             temperature=0.7,  # 生成する応答の多様性
             frequency_penalty=0.2,
             presence_penalty=0,
         )
-        response = response['choices'][0]['text'].replace("\n", "")
-        # 応答のテキスト部分を取り出して返す
-        return response
+
+        texts = ''.join([choice['text'] for choice in response.choices])
+        print(texts)
+
+        with open("./app/history.txt", 'w') as f:
+            f.write(prompt)
+            f.write(texts)
 
 
 if __name__ == "__main__":
     # AIChat のインスタンスを作成する
     chatai = AIChat()
-    print(chatai.response("あなたの名前は何ですか？"))
+    q = ""
+    q = input("こんにちは。御用はなんですか？\n")
+    chatai.response(q)
