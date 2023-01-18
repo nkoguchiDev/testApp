f900597e1ad456ce0de8a59135f4a50b2258720d
nkoguchi <nkoguchi00dev@gmail.com>
2023-01-18 22:19:00 +0900

    設定値をまとめ、プルリクエストのファイル差分を取得する関数を作成

--- git_manager/app/main.py
+++ git_manager/app/main.py
@@ -1,12 +1,27 @@
 import requests
 
 
-def get_pull_info(owner: str, repo: str, pull_num: int):
-    headers = {
+class Settings:
+    GIT_API_ENDPOINT: str = "https://api.github.com"
+    GIT_API_TOKEN: str = ""
+    GIT_API_HEADERS: dict = {
         "Accept": "application/vnd.github+json",
-        "Authorization": f"Bearer {settings.GIT_TOKEN}",
+        "Authorization": f"Bearer {GIT_API_TOKEN}",
         "X-GitHub-Api-Version": "2022-11-28"}
+
+
+settings = Settings()
+
+
+def get_pull_info(owner: str, repo: str, pull_num: int) -> list:
     pulls = requests.get(
-        f"https://api.github.com/repos/{owner}/{repo}/pulls/{pull_num}",
-        headers=headers)
-    return pulls
+        f"{settings.GIT_API_ENDPOINT}/repos/{owner}/{repo}/pulls/{pull_num}",
+        headers=settings.GIT_API_HEADERS)
+    return pulls.json()
+
+
+def get_pull_file_diff(owner: str, repo: str, pull_num: int) -> list:
+    pulls = requests.get(
+        f"{settings.GIT_API_ENDPOINT}/repos/{owner}/{repo}/pulls/{pull_num}/files",
+        headers=settings.GIT_API_HEADERS)
+    return pulls.json()
