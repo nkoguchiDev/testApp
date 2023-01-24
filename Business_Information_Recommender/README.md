5ab2395844a5885aa722ee97b4d95788c960504f
nkoguchi <nkoguchi00dev@gmail.com>
2023-01-24 23:37:40 +0900

    initial commit

---
+++ Business_Information_Recommender/README.md
@@ -0,0 +1,5 @@
+# Business Information Recommender
+
+## やりたいこと
+
+業務情報を蓄積できる
---
+++ Business_Information_Recommender/app/main.py
@@ -0,0 +1,6 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import Dataset
+import numpy as np
+import matplotlib.pyplot as plt
