05bdb995c0755c9d1cfc2c6e6884b928973a7e4c
nkoguchi <nkoguchi00dev@gmail.com>
2023-01-28 23:12:53 +0900

    リストが偶数でない場合は偶数に合わせる処理を追加

--- chat/chat/jobs/app/gen.py
+++ chat/chat/jobs/app/gen.py
@@ -86,6 +86,9 @@ if __name__ == "__main__":
     # 下記の処理を非同期で並列処理する
     # -> rayを使用して分散並列処理を行う
     voice_list = iter(split_message(text))
+    if len(voice_list) % 2 != 0:
+        generate_voice(voice_list[0])
+        del voice_list[0]
     ray.init(num_cpus=2)
     for i, j in zip(voice_list, voice_list):
         generate_voice.remote(i), generate_voice.remote(j)
